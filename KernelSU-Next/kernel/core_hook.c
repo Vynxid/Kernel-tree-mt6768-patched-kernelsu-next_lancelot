7814b258e49c7ff77a03022bd80bfd58f274d23a
Hicham Bchina <106634745+Edhic1@users.noreply.github.com>
2025-01-05 21:03:21 +0100

    Update core_hook.c

--- KernelSU-Next/kernel/core_hook.c
+++ KernelSU-Next/kernel/core_hook.c
@@ -922,8 +922,6 @@ out_ksu_try_umount:
 #endif
 	}
 
-// First definition (line 883)
-bool is_zygote_child = susfs_is_sid_equal(old->security, susfs_zygote_sid);
 
 	// check old process's selinux context, if it is not zygote, ignore it!
 	// because some su apps may setuid to untrusted_app but they are in global mount namespace
